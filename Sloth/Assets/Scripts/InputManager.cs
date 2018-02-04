d563f6bb2ca977a51615a3ff9993fbe57d1d19d2
Bryant <35882499+NfiniT-Studios@users.noreply.github.com>
2018-02-03 20:10:14 -0600

    Blocked In Level
    Added Blocked In Level to Prototype

---
+++ Sloth/Assets/Scripts/InputManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages player input, calling out appropriate functionality.
+/// </summary>
+public class InputManager : MonoBehaviour {
+    /// <summary>
+    /// Singleton reference to InputManager.
+    /// </summary>
+    static private InputManager singleton;
+    /// <summary>
+    /// Access singleton reference to InputManager
+    /// </summary>
+    static public InputManager Singleton
+    {
+        get
+        {
+            if (singleton == null)
+                singleton = FindObjectOfType<InputManager>();
+            return singleton;
+        }
+    }
+
+    /// <summary>
+    /// Early set-up.  Internal.  Establish singleton.
+    /// </summary>
+    private void Awake()
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Sloth/Assets/Scripts/UIManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    /// <summary>
+    /// Singleton reference to UIManager object
+    /// </summary>
+    static private UIManager singleton;
+    /// <summary>
+    /// Access singleton reference to UIManager object;
+    /// </summary>
+    static public UIManager Singleton
+    {
+        get
+        {
+            if (singleton == null)
+                singleton = FindObjectOfType<UIManager>();
+            return singleton;
+        }
+    }
+
+    /// <summary>
+    /// Early set-up.  Internal.  Establish singleton.
+    /// </summary>
+    private void Awake()
+    {
+        if (singleton == null)
+        {
+            singleton = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartNewGame()
+    {
+        //PRONTO: Add StartNewGame() functionality
+        SceneManager.LoadScene("Test", LoadSceneMode.Single);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();    
+#endif
+    }
+}
