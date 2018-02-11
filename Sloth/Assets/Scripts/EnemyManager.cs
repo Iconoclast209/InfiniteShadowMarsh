25ef065154941450eb3e2ac7f06ffe656ce12335
Bryant <35882499+NfiniT-Studios@users.noreply.github.com>
2018-02-10 19:11:10 -0600

    Basic AI Implemented
    Spiders now have Patrolling & Biting states and can attack Stanley Sloth.

--- Sloth/Assets/Scripts/EnemyManager.cs
+++ Sloth/Assets/Scripts/EnemyManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 
 /// <summary>
@@ -8,12 +9,177 @@
 /// </summary>
 public class EnemyManager : MonoBehaviour
 {
-    [Tooltip("Set enemy Health. Mobs: 5-50; Bosses: >50")]
+    [Header("Basic Enemy Stats")]
+    [Tooltip("Enemy starting health.")]
     [SerializeField]
     private int startingHealth;
 
+    [Tooltip("Patrol speed")]
+    [SerializeField]
+    private float movementSpeed = 2.0f;
+
+
+    [Tooltip("Enemy patrol radius")]
+    [SerializeField]
+    [Range(0.0f, 10.0f)]
+    private float patrolRadius = 5.0f;
+
+
+
+    [Tooltip("Should enemy start moving left?")]
+    [SerializeField]
+    private bool startMovingLeft;
+
+    [Tooltip("Enemy web-attack radius")]
+    [SerializeField]
+    [Range(0.0f, 10.0f)]
+    private float webRadius = 5.0f;
+
+    [Tooltip("Enemy web-attack delay (in seconds)")]
+    [SerializeField]
+    private float webDelay = 4.0f;
+
+    [Tooltip("Enemy bite-attack radius")]
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float biteRadius = 1.0f;
+
+    [Tooltip("Enemy bite-attack delay (in seconds)")]
+    [SerializeField]
+    private float biteDelay = 2.0f;
+
+    [Tooltip("Enemy bite-attack damage")]
+    [SerializeField]
+    private int biteDamage = 10;
+    
+    [Tooltip("Default State of Enemy")]
+    [SerializeField]
+    private State currentState;
+    public State remainInState;
+
     /// <summary>Current health of enemy.</summary>
     private int currentHealth;
+    /// <summary>Reference to enemy's Rigidbody2D component.</summary>
+    private Rigidbody2D rb;
+    /// <summary>Reference to enemy's BoxCollider2D component.</summary>
+    private BoxCollider2D bc;
+    /// <summary>Reference to enemy's starting position.</summary>
+    private float startingPosition;  
+    private float lastAttackTime;
+
+
+
+
+
+    public bool IsMovingLeft
+    {
+        get
+        {
+            return startMovingLeft;
+        }
+
+        set
+        {
+            startMovingLeft = value;
+        }
+    }
+
+    public float PatrolRadius
+    {
+        get
+        {
+            return patrolRadius;
+        }
+    }
+
+    public float StartingPosition
+    {
+        get
+        {
+            return startingPosition;
+        }
+        private set
+        {
+            startingPosition = value;
+        }
+    }
+
+    public Rigidbody2D RB
+    {
+        get
+        {
+            return rb;
+        }
+        private set
+        {
+            rb = value;
+        }
+    }
+
+    public float MovementSpeed
+    {
+        get
+        {
+            return movementSpeed;
+        }
+    }
+
+    public float BiteRadius
+    {
+        get
+        {
+            return biteRadius;
+        }
+
+        private set
+        {
+            biteRadius = value;
+        }
+    }
+
+    public float LastAttackTime
+    {
+        get
+        {
+            return lastAttackTime;
+        }
+
+        set
+        {
+            lastAttackTime = value;
+        }
+    }
+
+    public float BiteDelay
+    {
+        get
+        {
+            return biteDelay;
+        }
+
+        private set
+        {
+            biteDelay = value;
+        }
+    }
+
+    public int BiteDamage
+    {
+        get
+        {
+            return biteDamage;
+        }
+
+        private set
+        {
+            biteDamage = value;
+        }
+    }
+
+
+
+
+
 
     /// <summary>Damage enemy.  Kills enemy when appropriate.</summary>
     /// <param name="amountOfDamage">Amount of damage</param>
@@ -26,15 +192,90 @@ public class EnemyManager : MonoBehaviour
         }
     }
 
-    /// <summary>Destroy this object.</summary>
-    public void KillEnemy()
+
+
+
+
+    /// <summary>Kills enemy.</summary>
+    private void KillEnemy()
     {
-        Destroy(gameObject);
+
+            bc.enabled = false;
+            RB.velocity = new Vector2(0.0f, 5.0f);
+            RB.angularVelocity = 50.0f;
+            StartCoroutine(DieAfterSeconds(2.0f));
     }
 
     /// <summary>Early set-up.  Internal.</summary>
     private void Awake()
     {
+        ValidateVeriables();
+    }
+
+    private void ValidateVeriables()
+    {
         currentHealth = startingHealth;
+        StartingPosition = transform.position.x;
+        RB = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+
+        if (RB == null)
+            print("Spider does not have a Rigidbody2D component.");
+        if (bc == null)
+            print("Spider does not have a BoxCollider2D component.");
+    }
+
+    /// <summary>Kill enemy on collision with enemy</summary>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            KillEnemy();
+    }
+
+    /// <summary>Destroys enemy object after seconds.</summary>
+    private IEnumerator DieAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        currentState.UpdateState(this);
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        ShowPatrolInEditor();
+        ShowWebInEditor();
+        ShowBiteInEditor();
+    }
+
+    private void ShowPatrolInEditor()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(gameObject.transform.position + new Vector3(0.0f, 0.1f, 0.0f), gameObject.transform.position + new Vector3(PatrolRadius, 0.1f, 0f));
+        Gizmos.DrawLine(gameObject.transform.position + new Vector3(0.0f, 0.1f, 0.0f), gameObject.transform.position + new Vector3(-PatrolRadius, 0.1f, 0f));
+    }
+
+    private void ShowWebInEditor()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(gameObject.transform.position, webRadius);
+    }
+
+    private void ShowBiteInEditor()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(gameObject.transform.position, BiteRadius);
+    }
+
+    public void TransitionToState(State nextState)
+    {
+        if (nextState != remainInState)
+        {
+            currentState = nextState;
+        }
     }
 }
---
+++ Sloth/Assets/Scripts/ScriptableObjects/State.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "AI/State")]
+public class State : ScriptableObject {
+
+    [Tooltip("List of Actions taken in this state.")]
+    [SerializeField]
+    private Action[] actionsInState;
+
+    [Tooltip("List of Transitions in this state.")]
+    [SerializeField]
+    private Transition[] transitionsInState;
+
+    public void UpdateState(EnemyManager enemy)
+    {
+        DoActions(enemy);
+        CheckTransitions(enemy);
+    }
+
+    private void DoActions(EnemyManager enemy)
+    {
+        foreach (Action action in actionsInState)
+            action.Act(enemy);
+    }
+
+    private void CheckTransitions(EnemyManager enemy)
+    {
+        foreach(Transition transition in transitionsInState)
+        {
+            bool decisionSucceeded = transition.decision.Decide(enemy);
+
+            if (decisionSucceeded)
+                enemy.TransitionToState(transition.trueState);
+            else
+                enemy.TransitionToState(transition.falseState);
+        }        
+    }
+}
