25860dccb5856426dc4d94837f16f8341129d326
Bryant <35882499+NfiniT-Studios@users.noreply.github.com>
2018-02-15 18:42:34 -0600

    Working on GearTrain & Aart.

---
+++ Sloth/Assets/Scripts/Gear.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Gear : MonoBehaviour {
+    [Tooltip("Number of teeth on this gear")][SerializeField]
+    private int teethOnGear;
+
+    private float speed = 0.0f;
+    private bool clockwiseRotation;
+
+    public int TeethOnGear
+    {
+        get
+        {
+            return teethOnGear;
+        }
+
+        set
+        {
+            teethOnGear = value;
+        }
+    }
+
+    public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+
+        set
+        {
+            speed = value;
+        }
+    }
+
+    public bool ClockwiseRotation
+    {
+        get
+        {
+            return clockwiseRotation;
+        }
+
+        set
+        {
+            clockwiseRotation = value;
+        }
+    }
+
+    private void Update()
+    {
+        if (Speed >= 0.0f)
+        {
+            float newRotation = gameObject.transform.rotation.z + (Speed * Time.deltaTime);
+
+
+            gameObject.transform.Rotate(0f, 0f, newRotation);
+        }
+    }
+}
---
+++ Sloth/Assets/Scripts/GearTrain.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GearTrain : MonoBehaviour {
+
+    [Tooltip("Gear train to set")]
+    [SerializeField]
+    private Gear[] gearTrain;
+
+    [Tooltip("Speed to start with.")]
+    [SerializeField]
+    private float startingSpeed;
+
+    [Tooltip("Direction to start in.")]
+    [SerializeField]
+    private bool startClockwise;
+
+
+    private void Start ()
+    {
+        SetStartingSpeed();
+        if (gearTrain.Length > 1)
+            print(gearTrain.Length);
+            DetermineSpeedRatios(1);
+	}
+
+    private void SetStartingSpeed()
+    {
+        gearTrain[0].ClockwiseRotation = startClockwise;
+        gearTrain[0].Speed = startingSpeed;
+    }
+
+    private void DetermineSpeedRatios(int arrayElement)
+    {
+        float speedRatio = gearTrain[arrayElement].TeethOnGear / gearTrain[arrayElement - 1].TeethOnGear;
+        gearTrain[arrayElement].ClockwiseRotation = !gearTrain[arrayElement - 1].ClockwiseRotation;
+        gearTrain[arrayElement].Speed = gearTrain[arrayElement - 1].Speed * speedRatio;
+        print("Gear " + arrayElement + " speed: " + gearTrain[arrayElement].Speed);
+
+        if (gearTrain.Length - 1 > arrayElement)
+            DetermineSpeedRatios(arrayElement + 1);
+        return;
+    }
+}
