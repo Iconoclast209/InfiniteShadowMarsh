4fa872b52aa7d076bdc05a20abcdf002b408efd0
Bryant <35882499+NfiniT-Studios@users.noreply.github.com>
2018-02-18 12:58:36 -0600

    Menu SFX incorporated.  Need to DoNotDestroyOnLoad() the audio manager.

--- Sloth/Assets/Scripts/Button_StartGame.cs
+++ Sloth/Assets/Scripts/Button_StartGame.cs
@@ -1,10 +1,19 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Button_StartGame : MonoBehaviour, IPointerClickHandler
 {
     public void OnPointerClick(PointerEventData eventData)
     {
+        AudioManager.Singleton.MenuSelect();
+        StartCoroutine(StartGameWrapper());
+    }
+
+    private IEnumerator StartGameWrapper()
+    {
+        AudioManager.Singleton.MenuStartGame();
+        yield return new WaitForSeconds(AudioManager.Singleton.menuStartClips[0].length);
         UIManager.Singleton.StartNewGame();
     }
 }
